07dcf5848300b2cdc6e2948b873e388f1ed6e20f
fabio.vital <fabio-trab@outlook.com>
2025-02-20 02:56:41 -0400

    feat: add scope of game question page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GameQuestionComponent } from "./features/pages/game-question/game-question.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [GameQuestionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/features/pages/game-question/game-question.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-question',
+  imports: [],
+  templateUrl: './game-question.component.html',
+  styles: ``
+})
+export class GameQuestionComponent {
+
+}
